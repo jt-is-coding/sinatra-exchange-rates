51397357b0837a53cbf29303cee5cf7ae2176c5c
Jeremy Baxter <37813767+jt-is-coding@users.noreply.github.com>
2024-03-07 22:22:08 +0000

    just need to convert...

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
-exchange_rates_key = ENV.fetch("EXCHANGE_RATES_KEY")
+#exchange_rates_key = ENV.fetch("EXCHANGE_RATES_KEY")
 
 get("/") do
   
   erb(:main)
   
+end
+
+get("/:from") do
+  
+  @first_currency = params.fetch(:from)
+  erb(:from)
+
+end
+
+get("/:from/:to") do
+
+  @first_currency = params.fetch(:from)
+  @second_currency = params.fetch(:to)
+  erb(:to)
+
 end
